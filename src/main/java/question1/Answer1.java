b35f189a62efa28cd7471321325c9f82bf3c2fa3
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2022-07-30 00:27:19 +0530

    first commit

--- src/main/java/question1/Answer1.java
+++ src/main/java/question1/Answer1.java
@@ -3,9 +3,7 @@ package question1;
 public class Answer1 {
 
     // public static void main() {     // we can't make another method within a main method
-
-    private static String compareStringValues(String value)
-    {
+    private static String compareStringValues(final String value) {
 
         if (value.equals("MOM"))
             return "MOM";
@@ -13,9 +11,10 @@ public class Answer1 {
             return "DAD";
     }
 
-    public static void main(String[] args) {
-        System.out.println(compareStringValues("MOM"   +  "cook food for me"));
+    /**
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println(compareStringValues(null) + "cook food for me");
     }
-
 }
-//  there is no any Null point exception
